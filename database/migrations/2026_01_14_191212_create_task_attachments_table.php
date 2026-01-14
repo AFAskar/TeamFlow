41cdf823586169266726bd778f38c313879d4be8
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2026-01-14 22:25:23 +0300

    convert to UUID

--- database/migrations/2026_01_14_191212_create_task_attachments_table.php
+++ database/migrations/2026_01_14_191212_create_task_attachments_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('task_attachments', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->timestamps();
         });
     }
--- database/migrations/2026_01_14_191220_create_task_labels_table.php
+++ database/migrations/2026_01_14_191220_create_task_labels_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('task_labels', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->timestamps();
         });
     }
