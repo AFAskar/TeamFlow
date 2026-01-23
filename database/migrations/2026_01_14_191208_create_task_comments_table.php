14ad73dcf2bb38b134accdd041e18ab0facc6075
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2026-01-23 07:27:05 +0300

    self ref fix 2

--- database/migrations/2026_01_14_191208_create_task_comments_table.php
+++ database/migrations/2026_01_14_191208_create_task_comments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,10 +17,14 @@ return new class extends Migration
             $table->uuid('reply_to')->nullable();
             $table->uuid('created_by');
             $table->foreign('task_id')->references('id')->on('tasks')->cascadeOnDelete();
-            $table->foreign('reply_to')->references('id')->on('task_comments')->nullOnDelete();
+            // $table->foreign('reply_to')->references('id')->on('task_comments')->nullOnDelete(); // Moved to separate call
             $table->foreign('created_by')->references('id')->on('users')->cascadeOnDelete();
             $table->timestamps();
             $table->softDeletes();
+        });
+
+        Schema::table('task_comments', function (Blueprint $table) {
+            $table->foreign('reply_to')->references('id')->on('task_comments')->nullOnDelete();
         });
     }
 
